655f45aeb0071b98b3e45d2f0525f9f4cdd95425
Duncan Adamson <2073411a@gla.ac.uk>
2015-01-21 11:10:46 +0000

    Commiting from laptop for first time, no changes made

--- code/tango_with_django_project/rango/urls.py
+++ code/tango_with_django_project/rango/urls.py
@@ -1,3 +1,3 @@
-from django.comf.urls import patterns,url
-from randgo import views
-urlpatterns = patterns('',url(r'^$'.views.index,name='index'))
+from django.conf.urls import patterns,url
+from rango import views
+urlpatterns = patterns('',url(r'^$',views.index,name='index'),url(r'^about',views.about,name='about'))
