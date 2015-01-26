b2b20684a4c2e738b908ab45509544998cf42272
unknown <2073411a@BO715-8-03.ad.dcs.gla.ac.uk>
2015-01-26 13:41:02 +0000

    Finished Chapter 7

--- code/tango_with_django_project/rango/views.py
+++ code/tango_with_django_project/rango/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rango.models import Page, Category
+from rango.models import Page
+from rango.models import Category
 
 def index(request):
 	category_list = Category.objects.order_by('-likes')[:5]
-	context_dict = {'categories': category_list}
+	pages_list = Page.objects.order_by('-views')[:5]
+	context_dict = {'categories': category_list,'pages':pages_list}
 	return render(request, 'rango/index.html', context_dict)
 
 def about(request):
@@ -13,12 +15,12 @@ def about(request):
 
 def category(request, category_name_slug):
 	context_dict = {}
-    try:
+	try:
 		category = Category.objects.get(slug=category_name_slug)
-        context_dict['category_name'] = category.name
+		context_dict['category_name'] = category.name
 		pages = Page.objects.filter(category=category)
-        context_dict['pages'] = pages
-        context_dict['category'] = category
-    except Category.DoesNotExist:
-        pass
-    return render(request, 'rango/category.html', context_dict)
+		context_dict['pages'] = pages
+		context_dict['category'] = category
+	except Category.DoesNotExist:
+		pass
+	return render(request, 'rango/category.html', context_dict)
