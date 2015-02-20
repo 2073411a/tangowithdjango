9f15b30894d90a4eae822ef1f6cb7d0a3e039a98
Duncan Adamson <2073411a@gla.ac.uk>
2015-02-20 12:50:16 +0000

    Chapter 11-completed

--- code/tango_with_django_project/tango_with_django_project/urls.py
+++ code/tango_with_django_project/tango_with_django_project/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     # url(r'^$', 'tango_with_django_project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),url(r'^rango/', include('rango.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^rango/', include('rango.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += patterns(
